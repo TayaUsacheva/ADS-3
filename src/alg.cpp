8d37c1fb52af8f87c80c2d05b33a37d7f64931d1
TayaUsacheva <116427632+TayaUsacheva@users.noreply.github.com>
2023-03-18 18:29:21 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,26 +1,17 @@
 // Copyright 2021 NNTU-CS
-int sortirovka(int *arr, int size) {
-    for (int i = 0; i < size - 1; i++) {
-        for (int j = 0; j < size - i - 1; j++) {
-                if(arr[j] > arr[j + 1]) {
-                    int temp = arr[j + 1];
-                    arr[j + 1] = arr[j];
-                    arr[j] = temp;
-                }
-        }
-    }
-    return *arr;
-}
 int cbinsearch(int *arr, int size, int value) {
-  sortirovka(arr, size);
-  int center = 0;
+  int right = size - 1;
+  int left = 0;
+  int center = (right - left) / 2;
   int count_value = 0;
-  while (true) {
+  while (right - left > 0) {
     if (arr[center] > value) {
+        right = center - 1;
         center /= 2;
     } else if (arr[center] < value) {
         int x = center;
         center += (size - x) / 2;
+        left = x + 1;
     } else if (arr[center] == value) {
         count_value = 1;
         int i = center - 1;
